37499c8da8d2750d21b3f1b0402aa3114b62c158
Rushi <rushibhopale15@gmail.com>
2023-01-21 14:58:34 +0530

    Task Complete

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -3,16 +3,14 @@ package com.driver;
 public class Main {
   public static void main(String[] args) {
     DeluxePizza dp = new DeluxePizza(true);
-    System.out.println("Base Price for Pizza: "+dp.getPrice());
     dp.addTakeaway();
     dp.addExtraCheese();
     dp.addTakeaway();
     System.out.println(dp.getBill());
 
-    Pizza p = new Pizza(true);
+    Pizza p = new Pizza(false);
     p.addExtraCheese();
     p.addExtraToppings();
-    p.addTakeaway();
     System.out.println(p.getBill());
   }
 }
--- src/main/java/com/driver/Pizza.java
+++ src/main/java/com/driver/Pizza.java
@@ -6,35 +6,65 @@ public class Pizza {
     private Boolean isVeg;
     private String bill;
 
+    private int cheese;
+    private int toppings;
+    private boolean ischeese;
+    private boolean istoppings;
+    private boolean ispapaerbag;
+    private boolean isbill;
     public Pizza(Boolean isVeg){
-        this.isVeg = isVeg;
-        if(isVeg)this.price=300;
-        else this.price=400;
+       this.isVeg=isVeg;
+       if(isVeg)
+       {
+           this.price=300;
+           this.toppings=70;
+       }
+       else
+       {
+           this.price=400;
+           this.toppings=120;
+       }
+       this.cheese=80;
+        bill="Base Price Of The Pizza: "+price+"\n";
         // your code goes here
-    }
-
-    public int getPrice(){
-        return this.price;
     }
 
     public void addExtraCheese(){
-      this.price+=80;
-
+      if(!ischeese)
+      {
+          this.price+=this.cheese;
+          this.ischeese=true;
+      }
     }
 
     public void addExtraToppings(){
-        // your code goes here
-        if(isVeg) this.price+=70;
-        else this.price+=120;
+        if(!istoppings)
+        {
+            this.price+=this.toppings;
+            this.istoppings=true;
+        }
     }
 
     public void addTakeaway(){
-        // your code goes here
-        this.price+=20;
+       if(!ispapaerbag)
+       {
+           this.price+=20;
+           this.ispapaerbag=true;
+       }
     }
 
     public String getBill(){
         // your code goes here
-        return "Total Price: "+this.price;
+        //Base Price Of The Pizza: 300
+        if(!isbill) {
+            if (ischeese)
+                bill += "Extra Cheese Added: " + cheese + "\n";
+            if (istoppings)
+                bill += "Extra Toppings Added: " +toppings+ "\n";
+            if (ispapaerbag)
+                bill += "Paperbag Added: " +20+ "\n";
+            this.isbill=true;
+        }
+       return bill+="Total Price: "+price+"\n";
     }
 }
