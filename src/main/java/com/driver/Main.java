5d424a222d745af8404b2e07d8b2a05c5d1fd726
Rushi <rushibhopale15@gmail.com>
2023-01-21 15:37:25 +0530

    Task Completed

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -7,12 +7,12 @@ public class Main {
     dp.addTakeaway();
     dp.addExtraCheese();
     dp.addTakeaway();
-    System.out.println(dp.getBill());
+    System.out.print(dp.getBill());
 
     Pizza p = new Pizza(false);
     p.addExtraCheese();
     p.addExtraToppings();
-    //p.addTakeaway();
+    // p.addTakeaway();
     System.out.println(p.getBill());
   }
 }
--- src/main/java/com/driver/Pizza.java
+++ src/main/java/com/driver/Pizza.java
@@ -5,134 +5,75 @@ public class Pizza {
     private int price;
     private Boolean isVeg;
     private String bill;
-
     private int cheese;
     private int toppings;
-    private boolean ischeese;
-    private boolean istoppings;
+    boolean isCheese;
+    boolean isTopping;
+    boolean isTakeaway;
+    boolean isGenerated;
 
-    public int setPrice(int price) {
-        return this.price;
-    }
-
-    public Boolean getVeg() {
-        return isVeg;
-    }
-
-    public void setVeg(Boolean veg) {
-        isVeg = veg;
-    }
-
-    public void setBill(String bill) {
-        this.bill = bill;
-    }
-
-    public int getCheese() {
-        return cheese;
-    }
-
-    public void setCheese(int cheese) {
-        this.cheese = cheese;
-    }
-
-    public int getToppings() {
-        return toppings;
-    }
-
-    public void setToppings(int toppings) {
-        this.toppings = toppings;
-    }
-
-    public boolean isIscheese() {
-        return ischeese;
-    }
-
-    public void setIscheese(boolean ischeese) {
-        this.ischeese = ischeese;
-    }
-
-    public boolean isIstoppings() {
-        return istoppings;
-    }
-
-    public void setIstoppings(boolean istoppings) {
-        this.istoppings = istoppings;
-    }
-
-    public boolean isIspapaerbag() {
-        return ispapaerbag;
-    }
-
-    public void setIspapaerbag(boolean ispapaerbag) {
-        this.ispapaerbag = ispapaerbag;
-    }
-
-    public boolean isIsbill() {
-        return isbill;
-    }
-
-    public void setIsbill(boolean isbill) {
-        this.isbill = isbill;
-    }
-
-    private boolean ispapaerbag;
-    private boolean isbill;
     public Pizza(Boolean isVeg){
-       this.isVeg=isVeg;
-       if(isVeg)
-       {
-           this.price=300;
-           this.toppings=70;
-       }
-       else
-       {
-           this.price=400;
-           this.toppings=120;
-       }
-       this.cheese=80;
-        bill="Base Price Of The Pizza: "+price+"\n";
+        this.isVeg = isVeg;
         // your code goes here
+        this.isCheese=false;
+        this.isTopping=false;
+        this.isTakeaway=false;
+        this.isGenerated=false;
+        this.bill="";
+        if(isVeg){
+            this.price=300;
+            this.toppings=70;
+        }else{
+            this.price=400;
+            this.toppings=120;
+        }
+        this.cheese=80;
+        this.bill+="Base Price Of The Pizza: "+this.price+"\n";
     }
-    public int getPrice()
-    {
+
+    public int getPrice(){
         return this.price;
     }
+
     public void addExtraCheese(){
-      if(!ischeese)
-      {
-          this.price+=this.cheese;
-          this.ischeese=true;
-      }
+        // your code goes here
+        if(!isCheese){
+            this.price=this.price+cheese;
+            this.isCheese=true;
+        }
     }
 
     public void addExtraToppings(){
-        if(!istoppings)
-        {
-            this.price+=this.toppings;
-            this.istoppings=true;
+        // your code goes here
+        if(!isTopping){
+            this.price+=toppings;
+            this.isTopping=true;
         }
     }
 
     public void addTakeaway(){
-       if(!ispapaerbag)
-       {
-           this.price+=20;
-           this.ispapaerbag=true;
-       }
+        // your code goes here
+        if(!isTakeaway){
+            this.price+=20;
+            this.isTakeaway=true;
+        }
     }
 
     public String getBill(){
         // your code goes here
-        //Base Price Of The Pizza: 300
-        if(!isbill) {
-            if (ischeese)
-                bill += "Extra Cheese Added: " + cheese + "\n";
-            if (istoppings)
-                bill += "Extra Toppings Added: " +toppings+ "\n";
-            if (ispapaerbag)
-                bill += "Paperbag Added: " +20+ "\n";
-            this.isbill=true;
+        if(!isGenerated) {
+            if (isCheese) {
+                this.bill += "Extra Cheese Added: " + this.cheese + "\n";
+            }
+            if (isTopping) {
+                this.bill += "Extra Toppings Added: " + this.toppings + "\n";
+            }
+            if (isTakeaway) {
+                this.bill += "Paperbag Added: " + "20" + "\n";
+            }
+            this.bill += "Total Price: " + price + "\n";
+            isGenerated = true;
         }
-       return bill+="Total Price: "+price+"\n";
+        return this.bill;
     }
 }
